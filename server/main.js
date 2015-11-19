ce44e388bc6215b6727d79e57e3d76b2747c9d0b
yossieliyahu <yossi@montiera.com>
2015-11-19 17:20:36 +0200

    a

--- server/main.js
+++ server/main.js
@@ -6,16 +6,19 @@ var parser = require('body-parser');
 
 require('./database.js');
 
-app.use(express.static(__dirname + '/../.tmp'));
-app.use(express.static(__dirname + '/imgs'));
-app.use(parser.json());
-app.use(parser.urlencoded({extended:false}));
 
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
+
+app.use(express.static(__dirname + '/../.tmp'));
+app.use(express.static(__dirname + '/imgs'));
+app.use(parser.json());
+app.use(parser.urlencoded({extended:false}));
+
+
 
 
 app.listen(7777);
